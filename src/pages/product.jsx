94bcd54aeff52e779648a2f6d7196a873f5b54e6
omar tabbaa <omartabbaa0@gmail.com>
2025-02-15 18:39:10 +0100

    JSX procuct pagina

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -3,6 +3,26 @@ import React from 'react';
 const Product = () => {
     return (
         <div>
+            <div className="product-Photo-container">
+                <img src={header} alt="header" />
+            </div>
+
+            <div className="product-info-container">
+
+                <h2 className='product-title'>product-title</h2>
+                <a className='Mobiel-naam'>Mobiel-naam</a>
+            
+                <h1 className='product-prijs'>product-prijs</h1>
+                <div className='knop-container'>
+                    <button className='product-korting'>product-korting</button>
+                    <button className='product-winkelwagen'>product-winkelwagen</button>
+                </div>
+                <div className='product-beschrijving-container'>
+                    <p className='product-beschrijving'>product-beschrijving</p>
+                </div>
+             
+
+            </div>
             <h1>Product</h1>
         </div>
     )
