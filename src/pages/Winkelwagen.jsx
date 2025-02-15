4bcea4dc6fb72fbc39e56d62c4b1acd6c5899bc3
omar tabbaa <omartabbaa0@gmail.com>
2025-02-15 18:47:17 +0100

    JSX winkelwagen pagina

--- src/pages/Winkelwagen.jsx
+++ src/pages/Winkelwagen.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 
 const Winkelwagen = () => {
+
+
     return (
-        <div>
-            <h1>Winkelwagen</h1>
-        </div>
+       
+            <div className='winkelwagen-container'>
+                <div className="product-container">
+                    <img src={header} alt="header" />
+                    <h2 className='product-title'>product-title</h2>
+                    <a className='Mobiel-naam'>Mobiel-naam</a>
+                    <h1 className='product-prijs'>product-prijs</h1>
+               <button className='delete-button'>delete-button</button>
+                </div>
+                <div className="totaal-prijs-container">
+                    <h2 className='totaal-prijs'>totaal-prijs</h2>
+                    <h2 className='totaal-prijs'>totaal-prijs</h2>
+                    
+                </div>
+                <button className='afrekenen-button'>afrekenen-button</button>
+            </div>
+         
     )
 }   
 
